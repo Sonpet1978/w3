c08256e1098f8f8098f06b356ad5837eb3b11908
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-28 13:57:24 -0400

    add

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const PORT = 8080;
 
 // a function which handles requests and sends response
 const requestHandler = function(request, response) {
-  console.log('In requestHandler'); // NEW LINE
-  response.end(`Requested Path: ${request.url}\nRequest Method: ${request.method}`);
+    if (request.url === "/") {
+        response.end("Welcome!");
+      } else if (request.url === "/urls") {
+        response.end("www.lighthouselabs.ca\nwww.google.com");
+      } else {
+        response.statusCode = 404;
+        response.end("404 Page Not Found");
+      }
 };
 
 const server = http.createServer(requestHandler);
