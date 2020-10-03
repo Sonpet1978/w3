08aa0088d8f0c435b4f72ba15d3fa82d2b0b9f23
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-10-03 11:04:02 -0400

    add

--- durian-tree.js
+++ durian-tree.js
@@ -33,6 +33,32 @@ class Employee {
         return this.boss === employee.boss;
       }
 
+      employeesThatMakeOver(amount) {
+
+        let employees = []; // 1
+    
+        if (this.salary > amount) {
+          employees.push(this); // 2
+        }
+    
+        for (const subordinate of this.subordinates) {
+          const subordinatesThatMakeOver = subordinate.employeesThatMakeOver(amount); // 3
+          employees = employees.concat(subordinatesThatMakeOver);
+        }
+    
+        return employees;
+      }
+
+      get totalEmployees() {
+
+        let totalEmployees = 0; // 1
+    
+        // Use depth first traversal to calculate the total employees
+    
+        return totalEmployees;
+    
+      }
+
   }
 
 
@@ -47,3 +73,6 @@ const phil     = new Employee("Phil", "VP Marketing", 1000000);
 ada.addSubordinate(craig);
 ada.addSubordinate(angela);
 ada.addSubordinate(phil);
+
+
+let wealthyEmployees = ada.employeesThatMakeOver(418401);
